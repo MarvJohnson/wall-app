d4eff4a060179b5f420193f763e1c8b90e096799
Marvel Johnson <MarvelJohnson@Hotmail.com>
2022-09-20 18:26:52 -0700

    Added service interfaces and updated api
    -Added interface for getAllMessages
    -Added interface for sendMessage
    -Added interface for loginUser
    -Added interface for logoutUser
    -Added interface for registerUser
    -Updated backend view to use create_user helper method

--- backend/wallapp/urls.py
+++ backend/wallapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.get_all_messages, name='get_all_messages'),
-  path('feed/messages/new', views.create_message, name='create_message'),
+  path('messages', views.get_all_messages, name='get_all_messages'),
+  path('messages/new', views.create_message, name='create_message'),
   path('auth/register_user', views.register_new_user, name='register_new_user'),
   path('auth/login_user', views.login_user, name='login_user'),
   path('auth/logout_user', views.logout_user, name='logout_user'),
